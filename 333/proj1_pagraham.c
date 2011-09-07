016851fed31d12c41e9e87b6a119089a1cff993c
Peter Graham <peterghm@gmail.com>
2011-09-07 17:01:25 -0400

    basic hello world

---
+++ 333/proj1_pagraham.c
@@ -0,0 +1,14 @@
+/*
+ * CS333 Project 1 - C Programming
+ * Peter Graham
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  printf("kururururu.\n");
+  // other code here...
+  
+  return(0);
+}
