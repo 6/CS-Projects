139e9cb2760a0e5cc600348c7fa8e9bedb7d6b94
Peter Graham <peterghm@gmail.com>
2011-09-08 13:48:20 -0400

    make code more readable

--- 333/project1_pagraham/task1.c
+++ 333/project1_pagraham/task1.c
@@ -10,28 +10,28 @@ int main(int argc, char *argv[]) {
 }
 
 int task1() {
-  char type_char = 80;
-  short type_short = 22;
-  int type_int = 0x012345;
-  long type_long = 123456789101112;
-  float type_float = .123e3;
-  double type_double = 1234.5678;
+  char c = 80;
+  short s = 22;
+  int i = 0x012345;
+  long l = 123456789101112;
+  float f = .123e3;
+  double d = 1234.5678;
   //printf("%c %d %d %ld %f %f\n", type_char, type_short, type_int, type_long, type_float, type_double);
   
-  print_type_info("char", type_char, sizeof(type_char));
-  print_type_info("short", type_short, sizeof(type_short));
-  print_type_info("int", type_int, sizeof(type_int));
-  print_type_info("long", type_long, sizeof(type_long));
-  print_type_info("float", type_float, sizeof(type_float));
-  print_type_info("double", type_double, sizeof(type_double));
+  print_type_info("char", c, sizeof(c));
+  print_type_info("short", s, sizeof(s));
+  print_type_info("int", i, sizeof(i));
+  print_type_info("long", l, sizeof(l));
+  print_type_info("float", f, sizeof(f));
+  print_type_info("double", d, sizeof(d));
   
   return(0);
 }
 
-int print_type_info(char *desc, char* chr, int num_bytes) {
+int print_type_info(char *desc, char* type, int num_bytes) {
   printf("\n-------\n%s %d byte(s)\n", desc, num_bytes);
   
-  char* ptr = (char*) &chr;
+  char* ptr = (char*) &type;
   int i;
   for(i=0; i < num_bytes; i++){
     printf("%02x ", ptr[i]);
