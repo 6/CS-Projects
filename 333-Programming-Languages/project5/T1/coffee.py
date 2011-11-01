c7649dea83c09d3c865dd332ac7ca4194ab4e1b6
Peter Graham <peterghm@gmail.com>
2011-10-31 21:32:38 -0400

    start task 1 for coffeescript

---
+++ 333-Programming-Languages/project5/T1/coffee.py
@@ -0,0 +1,12 @@
+"""
+CS333 Task 1 - Sarah Harmon and Peter Graham
+
+Develop a semantic description of the Program function and the Initial State in
+CoffeeScript.
+"""
+
+def MProgram(p):
+  pass
+
+def MInitialState(decs):
+  pass
