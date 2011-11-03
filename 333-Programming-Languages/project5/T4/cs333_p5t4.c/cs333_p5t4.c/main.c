be9ca1426b5fc14e7d4e6336e95a4bc87a4f4bd3
Cheri Jones <cj.zaam@gmail.com>
2011-11-03 16:17:22 -0400

    Deleted first example.

--- 333-Programming-Languages/project5/T4/cs333_p5t4.c/cs333_p5t4.c/main.c
+++ 333-Programming-Languages/project5/T4/cs333_p5t4.c/cs333_p5t4.c/main.c
@@ -15,23 +15,8 @@ To run: in terminal
 
 int main (int argc, char *argv[])
 {
-	/* In this example, we attempt to capitalize the letters of the alphabet. */
-	
-	int ii = 0;
-	char* alphabet = "abcdefghijklmnopqrstuvwxyz";
-	
-	printf("%s\n", alphabet);
-	
-	for (ii=0; ii<25; ii++)
-	{
-		//this generates a bus error on my machine
-		alphabet[ii] = toupper(alphabet[ii]);	
-	}
-	
-	printf("%s\n", alphabet);
-	
 	/* In this example, we attempt to divide by zero */
-	printf("Let's try another example.");	//not printed	
+	printf("Let's try it!");	//not printed	
 	int x = 0;
 	
 	//this generates a floating point exception on my machine
