92d10076e49f93bb696a979653a146a528eec4d4
Peter Graham <peterghm@gmail.com>
2011-11-03 16:54:45 -0400

    add comments to program coffeescript

--- 333-Programming-Languages/project5/T1/program_coffee.py
+++ 333-Programming-Languages/project5/T1/program_coffee.py
@@ -10,5 +10,6 @@ def MProgram(program):
 
 def MInitialState():
   # CoffeeScript does not have an explicit declarations section like CLite.
-  # Variables can be declared anywhere in the program.
+  # Variables can be declared anywhere in the program. Therefore, return an
+  # empty symbol table.
   return {}
