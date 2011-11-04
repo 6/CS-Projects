6be7d89bd37c422f4697711283802161fb13abce
Peter Graham <peterghm@gmail.com>
2011-11-04 08:39:07 -0400

    forgot to commit - csharp conditional, foreach, while

--- 333-Programming-Languages/project5/T3/other_csharp.py
+++ 333-Programming-Languages/project5/T3/other_csharp.py
@@ -14,3 +14,25 @@ def MDo(d, state):
     return state
   state = MEmbeddedStatement(d.embeddedstatement, state)
   return MDo(d, state)
+
+def MIf(c, state):
+  if c.type == "ifelse":
+    if MBooleanExpression(c.booleanexpr, state):
+      return MEmbeddedStatement(c.embeddedstatement1, state)
+    else:
+      return MEmbeddedStatement(c.embeddedstatement2, state)
+  else: # "if" statement without an "else"
+    if MBooleanExpression(c.booleanexpr, state):
+      return MEmbeddedStatement(c.embeddedstatement, state)
+    else:
+      return state
+      
+def MForEach(f, state):
+  for identifier in MExpression(f.expr):
+    state = MEmbeddedStatement(f.embeddedstatement, state)
+  return state
+  
+def MWhile(w, state):
+  if not MBooleanExpression(w.booleanexpr, state):
+    return state
+  return MWhile(w, MEmbeddedStatement(w.embeddedstatement, state))
