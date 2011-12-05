75376681dd5ad70c235bd0f55bd900017ec3999f
Cheri Jones <cj.zaam@gmail.com>
2011-12-04 21:26:54 -0500

    CheckIt - init

---
+++ 333-Programming-Languages/project8/CheckIt/CheckIt/MainWindow.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MonoMac.Foundation;
+using MonoMac.AppKit;
+
+namespace CheckIt
+{
+	public partial class MainWindow : MonoMac.AppKit.NSWindow
+	{
+		#region Constructors
+		
+		// Called when created from unmanaged code
+		public MainWindow (IntPtr handle) : base (handle)
+		{
+			Initialize ();
+		}
+		
+		// Called when created directly from a XIB file
+		[Export ("initWithCoder:")]
+		public MainWindow (NSCoder coder) : base (coder)
+		{
+			Initialize ();
+		}
+		
+		// Shared initialization code
+		void Initialize ()
+		{
+			/*
+			NSToolbar toolbar = new NSToolbar("");
+			
+			
+			NSToolbarItem toolItem = new NSToolbarItem("Add");
+			//toolbar.InsertItem("+New Task", 0);
+			toolItem.Label = "+New Task";
+			toolItem.PaletteLabel = "Add New Task";
+			toolbar.InsertItem("Add",0);			
+			
+			*/
+		}
+	
+		#endregion
+	}
+}
+
